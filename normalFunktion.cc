9e76113b4c6286e0fbba3b7aef625741d6cd778c
hanan <hanan@hanans-MacBook-Pro.local>
2017-12-09 15:50:39 +0100

    my second wotk

---
+++ normalFunktion.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <random>
+#include "io.cc"
+#include "io.hh"
+
+
+
+
+
+int main ()
+{
+double avg = 5.00 ;
+double std_dev = 1.00 ;
+int N = 10 ;
+double min = 2.00 ;
+double max = 6.00 ;
+
+int seed = random_seed();
+
+
+
+std::vector<double>a=normal_distribution(5, 9,avg,std_dev) ;
+for(int i=0;i<a.size();i++)
+{
+std:: cout<<a[i];
+}
+
+}
